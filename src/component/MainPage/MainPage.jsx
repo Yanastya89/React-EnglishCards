065c6b52c9a3acdf5ace1b6411ea906d7c0aaddd
Kostrik Anastasiia <126889296+Yanastya89@users.noreply.github.com>
2024-02-26 13:35:52 +0900

    fix: structure

--- src/component/MainPage/MainPage.jsx
+++ src/component/MainPage/MainPage.jsx
@@ -5,15 +5,17 @@ export default function MainPage() {
   return (
     <main className={style.main}>
       <div className={style.cardWrapper}>
-        <div className={style.cardText}> {<CardSlider />} </div>
+        <div className={style.cardText}>
+          {" "}
+          <CardSlider />{" "}
+        </div>
         <img
           src="../../../public/images/Card.png"
           alt="Card"
           className={style.cardImage}
         />
       </div>
-
-      {<Table />}
+      <Table />
     </main>
   );
 }
