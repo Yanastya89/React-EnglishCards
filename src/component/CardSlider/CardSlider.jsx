ff75c2d58800ac9be1f91715779e988885c304a3
Kostrik Anastasiia <126889296+Yanastya89@users.noreply.github.com>
2024-03-31 20:16:32 +0900

    feat: сколько слов изучено , фокус на переводе

--- src/component/CardSlider/CardSlider.jsx
+++ src/component/CardSlider/CardSlider.jsx
@@ -6,6 +6,18 @@ import style from "../CardSlider/cardSlider.module.scss";
 function CardSlider() {
   const [countSlide, setCountSlide] = useState(0);
   const totalSlides = wordsData.length;
+  const [wordsLearned, setWordsLearned] = useState(0);
+  const [wordsAlreadyLearned, setWordsAlreadyLearned] = useState([]);
+
+  function handleLearned() {
+    if (!wordsAlreadyLearned[wordsData[countSlide].id]) {
+      setWordsLearned((prevCount) => prevCount + 1);
+      setWordsAlreadyLearned((prevLearned) => ({
+        ...prevLearned,
+        [wordsData[countSlide].id]: true,
+      }));
+    }
+  }
 
   function prevSlide() {
     setCountSlide((prevCount) => Math.max(0, prevCount - 1));
@@ -15,24 +27,63 @@ function CardSlider() {
     setCountSlide((prevCount) => Math.min(prevCount + 1, totalSlides - 1));
   }
 
+  function EndOfWord(wordsCount) {
+    const lastNumber = wordsCount % 10;
+    const lastTwoNumbers = wordsCount % 100;
+    switch (true) {
+      case lastTwoNumbers >= 11 && lastTwoNumbers <= 14:
+        return "слов";
+      case lastNumber === 1:
+        return "слово";
+      case lastNumber >= 2 && lastNumber <= 4:
+        return "слова";
+      default:
+        return "слов";
+    }
+  }
+
   return (
-    <div className={style.cardwrapper}>
-      <button
-        className={style.btn}
-        onClick={prevSlide}
-        disabled={countSlide === 0}
-      >
-        &lt;
-      </button>
-      <Card key={wordsData[countSlide].id} {...wordsData[countSlide]} />
-      <button
-        className={style.btn}
-        onClick={nextSlide}
-        disabled={countSlide === totalSlides - 1}
-      >
-        &gt;
-      </button>
-    </div>
+    <>
+      <div className={style.cardwrapper}>
+        <button
+          className={style.btn}
+          onClick={prevSlide}
+          disabled={countSlide === 0}
+        >
+          &lt;
+        </button>
+        <Card
+          key={wordsData[countSlide].id}
+          countslide={countSlide}
+          {...wordsData[countSlide]}
+          handleLearned={handleLearned}
+        />
+        <button
+          className={style.btn}
+          onClick={nextSlide}
+          disabled={countSlide === totalSlides - 1}
+        >
+          &gt;
+        </button>
+      </div>
+      <div className={style.count}>
+        <p>
+          {countSlide + 1}/{totalSlides}
+        </p>
+      </div>
+      <div className={style.info}>
+        <p>
+          Изучено: {wordsLearned} {EndOfWord(wordsLearned)}
+        </p>
+        <p>
+          {wordsLearned === totalSlides
+            ? "Вы изучили все слова!"
+            : `Осталось изучить ${totalSlides - wordsLearned} ${EndOfWord(
+                totalSlides - wordsLearned
+              )}`}
+        </p>
+      </div>
+    </>
   );
 }
 
