f91c9eec9cc7f58102e98f8654349c3be9cc1c72
Kostrik Anastasiia <126889296+Yanastya89@users.noreply.github.com>
2024-05-01 18:58:33 +0900

    fix: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./component/Header/Header";
 import Footer from "./component/Footer/Footer";
 import { Game, Main, Error, UserPage } from "./pages";
