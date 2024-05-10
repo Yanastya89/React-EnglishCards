2802dc91e4c23f7447b28285a5a53e13f10dcf39
Kostrik Anastasiia <126889296+Yanastya89@users.noreply.github.com>
2024-05-10 15:52:26 +0900

    feat: delete words

--- src/Context/MyContext.jsx
+++ src/Context/MyContext.jsx
@@ -23,6 +23,74 @@ export function MyContextComponent({ children }) {
     getWordsServer();
   }, []);
 
+  const addWord = async (newWord) => {
+    try {
+      const resp = await fetch(
+        "http://itgirlschool.justmakeit.ru/api/words/add",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newWord),
+        }
+      );
+      if (!resp.ok) {
+        throw new Error(`Failed to add word: ${resp.statusText}`);
+      }
+      const addedWord = await resp.json();
+      setDataServer([...dataServer, addedWord]);
+    } catch (error) {
+      console.error("Error adding word:", error);
+    }
+  };
+
+  const deleteWord = async (id) => {
+    try {
+      const resp = await fetch(
+        `http://itgirlschool.justmakeit.ru/api/words/${id}/delete`,
+        {
+          method: "POST",
+        }
+      );
+      if (!resp.ok) {
+        throw new Error(`Failed to delete word: ${resp.statusText}`);
+      }
+      setDataServer(dataServer.filter((word) => word.id !== id));
+    } catch (error) {
+      console.error("Error deleting word:", error);
+    }
+  };
+
+  const updateWord = async (id, updatedWord) => {
+    try {
+      const requestBody = {
+        english: updatedWord.english,
+        russian: updatedWord.russian,
+        transcription: updatedWord.transcription,
+        tags: updatedWord.tags || "",
+        id: updatedWord.id || "",
+        tags_json: updatedWord.tags_json || "",
+      };
+      const resp = await fetch(
+        `http://itgirlschool.justmakeit.ru/api/words/${id}/update`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestBody),
+        }
+      );
+      if (!resp.ok) {
+        throw new Error(`Failed to update word: ${resp.statusText}`);
+      }
+      await fetchDataFromServer();
+    } catch (error) {
+      console.error("Error updating word:", error);
+    }
+  };
+
   if (loading) {
     return (
       <div className={style.wrapper}>
@@ -32,7 +100,7 @@ export function MyContextComponent({ children }) {
     );
   }
 
-  const value = { dataServer, setDataServer };
+  const value = { dataServer, setDataServer, addWord, deleteWord, updateWord };
 
   return <MyContext.Provider value={value}> {children}</MyContext.Provider>;
 }
--- src/component/Table/Table.jsx
+++ src/component/Table/Table.jsx
@@ -1,46 +1,113 @@
 import React, { useContext, useEffect, useState } from "react";
 import { MyContext } from "../../Context/MyContext";
+import GetWordsService from "../../Services/GetWordsService";
 import style from "./table.module.scss";
 
-function Table({ deleteWords }) {
-  const { dataServer } = useContext(MyContext);
+function Table() {
+  const { dataServer, addWord, deleteWord, updateWord } = useContext(MyContext);
   const [editMode, setEditMode] = useState(false);
   const [editedFields, setEditedFields] = useState({});
   const [hasEmptyField, setHasEmptyField] = useState(false);
+  const [newWord, setNewWord] = useState({
+    english: "",
+    transcription: "",
+    russian: "",
+    tags: "",
+    id: "",
+    tags_json: "",
+  });
+
+  const fetchDataFromServer = async () => {
+    try {
+      const words = await GetWordsService.getWords();
+      setDataServer(words);
+    } catch (error) {
+      console.error("Ошибка при получении данных с сервера:", error);
+    }
+  };
+
+  const handleAddWord = async () => {
+    try {
+      await addWord(newWord);
+      await fetchDataFromServer();
+
+      setNewWord({
+        english: "",
+        transcription: "",
+        russian: "",
+        tags: "",
+        id: "",
+        tags_json: "",
+      });
+      setEditMode(false);
+
+      console.log("Слово успешно добавлено");
+    } catch (error) {
+      console.error("Ошибка при добавлении слова:", error);
+    }
+  };
+
+  const handleDeleteWord = async (id) => {
+    try {
+      await deleteWord(id);
+      await fetchDataFromServer();
+    } catch (error) {
+      console.error(`Failed to delete word with ID ${id}:`, error);
+    }
+  };
+
+  const handleUpdateWord = async (id, updatedFields) => {
+    try {
+      await updateWord(id, updatedFields);
+      await fetchDataFromServer();
+    } catch (error) {
+      console.error(`Failed to update word with ID ${id}:`, error);
+    }
+  };
 
   useEffect(() => {
-    const isEmpty = Object.values(editedFields).some(
+    const isEmpty = Object.values(newWord).some(
       (val) => typeof val === "string" && val.trim() === ""
     );
     setHasEmptyField(isEmpty);
-  }, [editedFields]);
+  }, [newWord]);
 
   const handleEdit = () => {
     setEditMode(true);
   };
 
   const validateFields = (fields) => {
-    return Object.values(fields).some(
-      (val) => typeof val === "string" && val.trim() !== ""
-    );
+    if (!fields || typeof fields !== "object") {
+      return false;
+    }
+    const fieldValues = Object.values(fields);
+    if (!Array.isArray(fieldValues)) {
+      return false;
+    }
+    return fieldValues.every((value) => value !== null && value !== undefined);
   };
-
-  const handleSave = () => {
-    const isValid = validateFields(editedFields);
-    if (isValid) {
-      console.log("Форма сохранена:", editedFields);
-      setEditMode(false);
-    } else {
-      console.log("Произошла ошибка. Заполните все поля перед сохранением.");
+  const handleSave = async () => {
+    try {
+      const isValid = validateFields(editedFields);
+      if (isValid) {
+        const idsToUpdate = Object.keys(editedFields);
+        await Promise.all(
+          idsToUpdate.map(async (id) => {
+            const updatedFields = editedFields[id];
+            await handleUpdateWord(id, updatedFields);
+          })
+        );
+        setEditMode(false);
+      } else {
+        console.log("Произошла ошибка. Заполните все поля перед сохранением.");
+      }
+    } catch (error) {
+      console.error("Ошибка при сохранении слова:", error);
     }
   };
 
   const handleCancel = () => {
     setEditMode(false);
-  };
-
-  const handleDelete = (id) => {
-    deleteWords(id);
   };
 
   const handleInputChange = (item, field, value) => {
@@ -49,17 +116,22 @@ function Table({ deleteWords }) {
       console.log("Строка не должна содержать цифр");
       return;
     }
-    setEditedFields((prevFields) => {
-      const updatedFields = {
-        ...prevFields,
-        [item.id]: { ...prevFields[item.id], [field]: value },
-      };
-      const isEmpty = Object.values(updatedFields).some(
-        (field) => typeof field === "string" && field.trim() === ""
-      );
-      setHasEmptyField(isEmpty);
-      return updatedFields;
-    });
+
+    const updatedFields = {
+      ...editedFields,
+      [item.id]: {
+        ...editedFields[item.id],
+        [field]: value,
+      },
+    };
+
+    setEditedFields(updatedFields);
+
+    const isEmpty = Object.values(updatedFields).some(
+      (field) => typeof field === "string" && field.trim() === ""
+    );
+
+    setHasEmptyField(isEmpty);
   };
 
   return (
@@ -69,7 +141,12 @@ function Table({ deleteWords }) {
           <th>english</th>
           <th>transcription</th>
           <th>russian</th>
-          <th></th>
+          <th>
+            {" "}
+            <button onClick={handleAddWord} className={style.addWordButton}>
+              Add word
+            </button>
+          </th>
         </tr>
       </thead>
       <tbody>
@@ -152,7 +229,7 @@ function Table({ deleteWords }) {
                 <>
                   <button
                     className={`${style.saveButton} ${style.btn}`}
-                    onClick={handleSave}
+                    onClick={() => handleSave(item.id)}
                     disabled={hasEmptyField}
                   ></button>
                   <button
@@ -168,7 +245,7 @@ function Table({ deleteWords }) {
                   ></button>
                   <button
                     className={`${style.deleteButton} ${style.btn}`}
-                    onClick={() => handleDelete(item.id)}
+                    onClick={() => handleDeleteWord(item.id)}
                   ></button>
                 </>
               )}
